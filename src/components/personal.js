31b83c894bd2c93c43a695bb9c12ee8611836710
CaolanCode <cmacmahon3@gmail.com>
2023-04-23 15:10:10 +0100

    commit personal inputs

--- src/components/personal.js
+++ src/components/personal.js
@@ -3,31 +3,18 @@ import React from "react";
 export default function Personal() {
   return (
     <div className="personal-form">
-      <div className="left-personal">
-        <div>First Name</div>
-        <div>Surname</div>
-        <div>Current Role</div>
-      </div>
-      <div className="right-personal">
-        <div>Phone</div>
-        <div>Email</div>
-        <div>LinkedIn</div>
-      </div>
+      <label>First Name</label>
+      <input placeholder="First Name"/>
+      <label>Surname</label>
+      <input placeholder="Surname"/>
+      <label>Current Role</label>
+      <input placeholder="Current Role"/>
+      <label>Phone</label>
+      <input placeholder="Phone" type="tel"/>
+      <label>Email</label>
+      <input placeholder="Email" type="email"/>
+      <label>LinkedIn</label>
+      <input placeholder="LinkedIn"/>
     </div>
   )
 }
-
-/*
-  <label>First Name</label>
-  <input placeholder="First Name"/>
-  <label>Surname</label>
-  <input placeholder="Surname"/>
-  <label>Current Role</label>
-  <input placeholder="Current Role"/>
-  <label>Phone</label>
-  <input placeholder="Phone" type="tel"/>
-  <label>Email</label>
-  <input placeholder="Email" type="email"/>
-  <label>LinkedIn</label>
-  <input placeholder="LinkedIn"/>
-*/
