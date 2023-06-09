14d56703565e6b5c92923b48d2d52046656c55cb
CaolanCode <cmacmahon3@gmail.com>
2023-06-09 16:54:46 +0100

    display personal info with no styling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-import React, {Component} from "react"
+import React, {useRef} from "react"
 import Personal from "./components/Personal"
 import WorkEduTitle from "./components/WorkEduTitle"
 import Skill from "./components/Skills"
 import './style.css'
 
-class App extends Component {
-  constructor() {
-    super()
+export default function App() {
+  const personalRef = useRef(null)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if(personalRef.current) {
+      personalRef.current.displayPersonal()
+    }
   }
 
-  render() {
-    return (
-      <div className="form">
-        <Personal />
-        <WorkEduTitle  title="Work Experience" cls="work-container" />
-        <WorkEduTitle title="Education" cls="edu-container" />
-        <Skill />
-        <div className="btn-container">
-          <button id="submit-btn">Submit</button>
-        </div>
+
+  return (
+    <form className="form">
+      <Personal ref={personalRef}/>
+      <WorkEduTitle  title="Work Experience" cls="work-container" />
+      <WorkEduTitle title="Education" cls="edu-container" />
+      <Skill />
+      <div className="btn-container">
+        <button id="submit-btn" type="submit" onClick={handleClick}>Submit</button>
       </div>
-    )
-  }
+    </form>
+  )
 }
-
-export default App
--- src/components/personal.js
+++ src/components/personal.js
@@ -1,12 +1,13 @@
-import React, {useState} from "react";
+import React, {forwardRef, useImperativeHandle, useState} from "react";
 
-export default function Personal() {
+const Personal = forwardRef((props, ref) => {
   const [firstname, setFirstname] = useState('')
   const [surname, setSurname] = useState('')
   const [role, setRole] = useState('')
   const [phone, setPhone] = useState('')
   const [email, setEmail] = useState('')
   const [linkedin, setLinkedin] = useState('')
+  const [showForm, setShowForm] = useState(true)
 
   const handleFirstnameChange = (e) => {
     setFirstname(e.target.value)
@@ -27,20 +28,45 @@ export default function Personal() {
     setLinkedin(e.target.value)
   }
 
+  const displayPersonal = () => {
+    setShowForm(false)
+  }
+
+  useImperativeHandle(ref, () => ({
+    displayPersonal: displayPersonal
+  }))
+
   return (
-    <div className="personal-form section">
-      <label className="input-label">First Name</label>
-      <input placeholder="First Name" value={firstname} onChange={handleFirstnameChange}/>
-      <label className="input-label">Surname</label>
-      <input placeholder="Surname" value={surname} onChange={handleSurnameChange}/>
-      <label className="input-label">Current Role</label>
-      <input placeholder="Current Role" value={role} onChange={handleRoleChange}/>
-      <label className="input-label">Phone</label>
-      <input placeholder="Phone" type="tel" value={phone} onChange={handlePhoneChange}/>
-      <label className="input-label">Email</label>
-      <input placeholder="Email" type="email" value={email} onChange={handleEmailChange}/>
-      <label className="input-label">LinkedIn</label>
-      <input placeholder="LinkedIn" value={linkedin} onChange={handleLinkedinChange}/>
+    <div>
+      {
+        showForm ? (
+          <div className="personal-form section">
+            <label className="input-label">First Name</label>
+            <input placeholder="First Name" value={firstname} onChange={handleFirstnameChange}/>
+            <label className="input-label">Surname</label>
+            <input placeholder="Surname" value={surname} onChange={handleSurnameChange}/>
+            <label className="input-label">Current Role</label>
+            <input placeholder="Current Role" value={role} onChange={handleRoleChange}/>
+            <label className="input-label">Phone</label>
+            <input placeholder="Phone" type="tel" value={phone} onChange={handlePhoneChange}/>
+            <label className="input-label">Email</label>
+            <input placeholder="Email" type="email" value={email} onChange={handleEmailChange}/>
+            <label className="input-label">LinkedIn</label>
+            <input placeholder="LinkedIn" value={linkedin} onChange={handleLinkedinChange}/>
+          </div>
+        ) : (
+          <div>
+            <div>{firstname} {surname}</div>
+            <div>{role}</div>
+            <div>{phone}</div>
+            <div>{email}</div>
+            <div>{linkedin}</div>
+          </div>
+        )
+      }
     </div>
+
   )
-}
+
+})
+export default Personal
