4f466fdc1bcc20e7859dadde4e445a3b55bf0f75
CaolanCode <cmacmahon3@gmail.com>
2023-06-09 12:58:01 +0100

    store personal input values

--- src/components/personal.js
+++ src/components/personal.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Personal() {
+  const [firstname, setFirstname] = useState('')
+  const [surname, setSurname] = useState('')
+  const [role, setRole] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [linkedin, setLinkedin] = useState('')
+
+  const handleFirstnameChange = (e) => {
+    setFirstname(e.target.value)
+  }
+  const handleSurnameChange = (e) => {
+    setSurname(e.target.value)
+  }
+  const handleRoleChange = (e) => {
+    setRole(e.target.value)
+  }
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value)
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleLinkedinChange = (e) => {
+    setLinkedin(e.target.value)
+  }
+
   return (
     <div className="personal-form section">
       <label className="input-label">First Name</label>
-      <input placeholder="First Name"/>
+      <input placeholder="First Name" value={firstname} onChange={handleFirstnameChange}/>
       <label className="input-label">Surname</label>
-      <input placeholder="Surname"/>
+      <input placeholder="Surname" value={surname} onChange={handleSurnameChange}/>
       <label className="input-label">Current Role</label>
-      <input placeholder="Current Role"/>
+      <input placeholder="Current Role" value={role} onChange={handleRoleChange}/>
       <label className="input-label">Phone</label>
-      <input placeholder="Phone" type="tel"/>
+      <input placeholder="Phone" type="tel" value={phone} onChange={handlePhoneChange}/>
       <label className="input-label">Email</label>
-      <input placeholder="Email" type="email"/>
+      <input placeholder="Email" type="email" value={email} onChange={handleEmailChange}/>
       <label className="input-label">LinkedIn</label>
-      <input placeholder="LinkedIn"/>
+      <input placeholder="LinkedIn" value={linkedin} onChange={handleLinkedinChange}/>
     </div>
   )
 }
